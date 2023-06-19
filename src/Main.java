aed1748d4ee9edad38e74eee5f270d0edf8f4dff
Angelisium <angelisium@daum.net>
2023-06-19 16:00:27 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,7 +20,7 @@ public class Main {
 	private static final String POSTGRES_USERNAME = "root";
 	private static final String POSTGRES_PASSWORD = "root";
 
-	// private static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String MYSQL_URL = "jdbc:mysql://localhost:3306?useSSl=false";
 	private static final String MYSQL_USERNAME = "root";
 	private static final String MYSQL_PASSWORD = "";
@@ -29,11 +32,62 @@ public class Main {
 				Class.forName(POSTGRES_DRIVER);
 				db = DriverManager.getConnection(POSTGRES_URL, POSTGRES_USERNAME, POSTGRES_PASSWORD);
 			} else {
-				Class.forName("com.mysql.cj.jdbc.Driver");
+				Class.forName(MYSQL_DRIVER);
 				db = DriverManager.getConnection(MYSQL_URL, MYSQL_USERNAME, MYSQL_PASSWORD);
 			}
+			Statement statement = db.createStatement();
 
+			// créé la db si elle n'existe pas
+			statement.executeUpdate("CREATE DATABASE IF NOT EXISTS TPJDBCJAVA CHARACTER SET utf8;");
+			statement.executeUpdate("use TPJDBCJAVA;");
+			// Utilisateurs
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS Utilisateurs (" +
+					"id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+					"number VARCHAR(50) NOT NULL," +
+					"lastname VARCHAR(50) NOT NULL," +
+					"firstname VARCHAR(50) NOT NULL," +
+					"email VARCHAR(255) NOT NULL," +
+					"login VARCHAR(50) NOT NULL," +
+					"password VARCHAR(50) NOT NULL" +
+					");");
+			// Clients
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS Clients (" +
+					"id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+					"number VARCHAR(50) NOT NULL," +
+					"lastname VARCHAR(50) NOT NULL," +
+					"firstname VARCHAR(50) NOT NULL," +
+					"email VARCHAR(255) NOT NULL," +
+					"adresse VARCHAR(255) NOT NULL" +
+					");");
+			// Fournisseurs
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS Fournisseurs (" +
+					"id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+					"number VARCHAR(50) NOT NULL," +
+					"lastname VARCHAR(50) NOT NULL," +
+					"email VARCHAR(255) NOT NULL," +
+					"adresse VARCHAR(255) NOT NULL" +
+					");");
+			// Articles
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS Articles (" +
+					"id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+					"number VARCHAR(50) NOT NULL," +
+					"sold INT NOT NULL," + // vendu, oui / non
+					"name VARCHAR(50) NOT NULL," +
+					"description VARCHAR(255) NOT NULL" +
+					");");
+
+			// statement.executeUpdate("INSERT INTO Utilisateurs VALUES (DEFAULT, 'Client
+			// n°1', 'Lastname', 'Firstname', 'monsuper@email.com', 'monSuperLogin',
+			// 'fakePassword')");
 			// connection effectuer, opération ici
+			ResultSet result = statement.executeQuery("SELECT * FROM Utilisateurs;");
+			ResultSetMetaData meta = result.getMetaData();
+
+			System.out.println("table name : " + meta.getTableName(1));
+			for (int i = 0; i < meta.getColumnCount(); i++) {
+				System.out.println(meta.getColumnName(i + 1));
+			}
+
 			System.out.println("Hello, World!");
 
 			db.close();
