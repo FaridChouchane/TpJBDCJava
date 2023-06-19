e1c4db4788037f30a9a16873df233b20e247137e
Angelisium <angelisium@daum.net>
2023-06-19 10:41:36 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
+	private static final String POSTGRES_URL = "jdbc:postgresql://127.0.0.1:5432/base";
+	private static final String POSTGRES_USERNAME = "admin";
+	private static final String POSTGRES_PASSWORD = "root";
+
+	private static final String MYSQL_URL = "**";
+	private static final String MYSQL_USERNAME = "**";
+	private static final String MYSQL_PASSWORD = "";
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
